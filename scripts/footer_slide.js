a94a1406de60b53c0ee4ed6b310e1ee53bf3a8ad
Reyog paneru <155659738+Reyog12@users.noreply.github.com>
2024-01-28 00:51:00 +0545

    scripts

---
+++ scripts/footer_slide.js
@@ -0,0 +1,14 @@
+const nav_links = document.querySelectorAll(".js-nav_link");
+const footer = document.querySelector(".js-footer");
+
+[...nav_links].forEach((element) => {
+  element.addEventListener("focusin", () => {
+    footer.classList.add("slide");
+  });
+  element.addEventListener("focusout", () => {
+    footer.classList.remove("slide");
+  });
+});
+$('#filtersubmit').click(function() { 
+  alert('Searching for '+$('#filter').val());
+});
